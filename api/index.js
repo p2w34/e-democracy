55c52a365b7b60714d2c8353a84d70e1f92d92d3
Paweł Broda <pwl.broda@gmail.com>
2024-02-01 20:58:09 +0100

    refactor API Lambda body

--- api/index.js
+++ api/index.js
@@ -3,46 +3,47 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
     try {
-        // Extract the original file name from the Content-Disposition header, if available
-        const contentDisposition = event.headers['content-disposition'];
-        const originalFileNameMatch = contentDisposition ? /filename="(.+?)"/.exec(contentDisposition) : null;
+        const pdfBuffer = extractPdfBuffer(event);
+        const fileName = extractFileName(event);
+        await s3Upload(pdfBuffer, fileName);
 
-        // If the original file name is found in the header, use it; otherwise, use a default value
-        const originalFileName = originalFileNameMatch ? originalFileNameMatch[1] : `file-${Date.now()}.pdf`;
+        console.log(`File uploaded successfully`);
 
-        // The event body is expected to be a base64-encoded string of the PDF file
-        const pdfBuffer = Buffer.from(event.body, 'base64');
-        const bucketName = 'e-petitions'; // Replace with your bucket name
-
-        // Use the original file name as the key for the S3 object
-        const key = `uploads/${originalFileName}`;
-
-        // Parameters for S3 upload
-        const params = {
-            Bucket: bucketName,
-            Key: key,
-            Body: pdfBuffer,
-            ContentType: 'application/pdf'
-        };
-
-        // Uploading the PDF to S3
-        await s3.putObject(params).promise();
-
-        // Logging the event
-        console.log(`File uploaded successfully: ${key}`);
-
-        // Return a 200 response
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'File uploaded successfully' })
         };
     } catch (error) {
-        console.error('Error uploading file:', error);
+        console.error('Error:', error);
 
-        // Return a 500 response on error
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error uploading file' })
+            body: JSON.stringify({ error: 'Error occurred' })
         };
     }
 };
+
+function extractPdfBuffer(event) {
+    return Buffer.from(event.body, 'base64');
+}
+
+function extractFileName(event) {
+    const contentDisposition = event.headers['content-disposition'];
+    const originalFileNameMatch = contentDisposition ? /filename="(.+?)"/.exec(contentDisposition) : null;
+    const originalFileName = originalFileNameMatch ? originalFileNameMatch[1] : `file-${Date.now()}.pdf`;
+    return { originalFileName };
+}
+
+async function s3Upload(pdfBuffer, { originalFileName }) {
+    const key = `uploads/${originalFileName}`;
+
+    const params = {
+        Bucket: 'e-petitions',
+        Key: key,
+        Body: pdfBuffer,
+        ContentType: 'application/pdf'
+    };
+
+    await s3.putObject(params).promise();
+    console.log(`File uploaded successfully: ${key}`);
+}
