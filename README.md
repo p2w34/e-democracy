62070e56483b67517b2e043b6721f17cc3e55c96
Paweł Broda <pwl.broda@gmail.com>
2024-02-02 17:57:34 +0100

    add similarity to template and signature validation

--- README.md
+++ README.md
@@ -21,11 +21,12 @@
 1. API GATEWAY (HTTP)
 - might be a problem due to CORS; then set it properly on AWS, for testing I set Access-Control-Allow-Origin as * and Access-Control-Allow-Headers as *, and Access-Control-Allow-Methods as POST
 2. S3 Bucket (enable versioning to err on the safe side)
-3. Lambda. To prepare .zip file for AWS Lambda:  
-  a. `cd www/`  
-  b. `npm install` # creates node_modules/ based on package json and package-lock.json files  
-  c. `zip -r e-petitions-api.zip .`
-  d. on AWS console, in: "Edit runtime settings" in the   handler name put index.petitions ([name of lambda file].[name of exported method])  
+3. Lambda. To prepare .zip file for AWS Lambda:
+  a   if .zip file too large (>50MB, upload to an S3 bucket first)  
+  b. `cd www/`  
+  c. `npm install` # creates node_modules/ based on package json and package-lock.json files  
+  d. `zip -r e-petitions-api.zip .`
+  e. on AWS console, in: "Edit runtime settings" in the   handler name put index.petitions ([name of lambda file].[name of exported method])  
 4. IAM. In the Lambda config page see the role and add to it following policy:
 ```
 {
@@ -50,13 +51,37 @@
 One can for sure use the most simple approach, which is go with typical hacking backed by:
 - `npm init -y` # to set up nodejs project from scratch
 - `npm install -g nodemon` # to reload automatically changes to server.js etc.
-- `aws lambda update-function-code --function-name e-petitions - zip-file fileb://e-petitions-api.zip` # to upload Lambda code to the exsiting 'e-petitions' lambda
+- `aws lambda update-function-code --function-name e-petitions --zip-file fileb://e-petitions-api.zip` # to upload Lambda code to the exsiting 'e-petitions' lambda
 
 It seems though the recommended way is to use tools which allow to run Lambda locally. Lambda running this way, when aws cli configured properly, is able to hit other parts of S3 infra (actual parts, not something emulated locally).
 However, the below install lots of packages so that the resulting .zip Lambda file is around 50MB (which is limit when uploading manually from AWS WWW console). Hence recommended for dev purposes only. Also, 10x longer lambda execution times were observed!
 - `npm install -g serverless` # install
 - `npm install --save-dev serverless-offline` # install
 - `sls offline` # start a server exposing API defined as Lambda
+
+To debug whole setup (Lambda run locally with serverless + curl):  
+a. `npm install nodemon --save-dev`  
+b. configure nodemon. In your package.json, set up a nodemon script. This script should start Serverless Offline and enable Node.js debugging with the --inspect flag:
+```
+"scripts": {
+  "debug": "nodemon --exec 'node --inspect ./node_modules/serverless/bin/serverless offline'"
+}
+```
+c. In VSCode set up a launch configuration for debugging: 'run -> add configuration':
+```
+{
+  "type": "node",
+  "request": "attach",
+  "name": "Attach to Serverless",
+  "port": 9229,
+  "restart": true,
+  "timeout": 10000
+}
+
+```
+
+Add a configuration for attaching to a Node.js process:
+d. `npm run debug` or 'run->start debuggin in VSCode' to start debugging
 
 FYI, 'serverless.yaml' config file was created with "serverless create --template aws-nodejs"
 
--- api/index.js
+++ api/index.js
@@ -1,26 +1,60 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const pdfParse = require('pdf-parse');
+const stringSimilarity = require('string-similarity');
+const verifyPDF = require('@ninja-labs/verify-pdf'); // installed with: npm i @ninja-labs/verify-pdf --legacy-peer-deps
+
 const s3 = new AWS.S3();
 
 exports.petitions = async (event) => {
+    let pdfBuffer;
+    let fileName;
     try {
-        const pdfBuffer = extractPdfBuffer(event);
-        const fileName = extractFileName(event);
-        await s3Upload(pdfBuffer, fileName);
-
-        console.log(`File uploaded successfully`);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'File uploaded successfully' })
-        };
+        pdfBuffer = extractPdfBuffer(event);
+        fileName = extractFileName(event);
     } catch (error) {
         console.error('Error:', error);
-
         return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error occurred' })
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Error occurred, cannot parse file',
+                errorMessage: error.message
+            })
         };
     }
+   
+    try {
+        validateFileProvided(pdfBuffer);
+        await validateFileSimilarity('petycja_wzor.pdf', pdfBuffer);
+        await validateFileSignature(pdfBuffer);
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Error occurred, cannot validate file',
+                errorMessage: error.message
+            })
+        };
+    }
+    
+    try {
+        await s3Upload(pdfBuffer, fileName);
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: 'Error occurred, cannot store file',
+                errorMessage: error.message
+            })
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'File uploaded successfully' })
+    };
 };
 
 function extractPdfBuffer(event) {
@@ -32,6 +66,52 @@ function extractFileName(event) {
     const originalFileNameMatch = contentDisposition ? /filename="(.+?)"/.exec(contentDisposition) : null;
     const originalFileName = originalFileNameMatch ? originalFileNameMatch[1] : `file-${Date.now()}.pdf`;
     return { originalFileName };
+}
+
+function validateFileProvided(pdfBuffer) {
+    if (!pdfBuffer) {
+        throw new Error('No file provided');
+    }
+}
+
+async function validateFileSimilarity(templateFilePath, pdfBuffer) {
+    try {
+        const data = await pdfParse(pdfBuffer);
+
+        const templateBuffer = fs.readFileSync(templateFilePath);
+        const templateData = await pdfParse(templateBuffer);
+
+        const similarity = calculatePdfSimilarity(data.text, templateData.text);
+
+        console.log('similarity: ' + similarity);
+        if (similarity < 80) {
+            throw new Error('PDF similarity less than 80%');
+        }
+    } catch (error) {
+        throw new Error('Error validating PDF similarity');
+    }    
+}
+
+async function validateFileSignature(signedPdfBuffer) {
+    
+    try {
+        const {
+            verified,
+            authenticity,
+            integrity,
+            expired,
+            signatures
+        } = verifyPDF(signedPdfBuffer);
+
+    } catch (error) {
+        throw new Error('Error validating PDF signature');
+    }  
+}
+
+function calculatePdfSimilarity(text1, text2) {
+    const similarity = stringSimilarity.compareTwoStrings(text1, text2);
+    const similarityPercentage = (similarity * 100).toFixed(2); 
+    return similarityPercentage;
 }
 
 async function s3Upload(pdfBuffer, { originalFileName }) {
